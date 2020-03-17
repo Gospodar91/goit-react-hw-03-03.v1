4c5b3321bc641bd77be20ab3abdfee3070f78670
Sergei <xsergeix@ukr.net>
2020-03-17 12:00:15 +0300

    Updates

---
+++ precache-manifest.47cfe6bc57ba764210e9a665c7fbe982.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8c6e6d4c029f3db967707cb9f2ef92",
+    "url": "/goit-react-hw-03-03.v1/index.html"
+  },
+  {
+    "revision": "8c0377f81c84a7eeec99",
+    "url": "/goit-react-hw-03-03.v1/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "e0d14a10dc06db342daa",
+    "url": "/goit-react-hw-03-03.v1/static/css/main.beb9692f.chunk.css"
+  },
+  {
+    "revision": "8c0377f81c84a7eeec99",
+    "url": "/goit-react-hw-03-03.v1/static/js/2.ad616bc8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-03.v1/static/js/2.ad616bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d14a10dc06db342daa",
+    "url": "/goit-react-hw-03-03.v1/static/js/main.dd1e8b33.chunk.js"
+  },
+  {
+    "revision": "b9c6409e8ac7af5171dd",
+    "url": "/goit-react-hw-03-03.v1/static/js/runtime-main.bae9ca05.js"
+  }
+]);
